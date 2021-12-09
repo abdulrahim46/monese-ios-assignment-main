cd9b417b1377cc1707ed27aa15d43e2903d20f75
abdulrahim46 <abdul7716@gmail.com>
2021-12-09 19:37:31 +0530

    Initial commit

---
+++ Assignment/Assignment/Home/HomeInteractor.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+class HomeInteractor {
+    var presenter: HomeInteractorOutput?
+    let repository: LaunchRepository = LaunchRepositoryImpl()
+}
+
+extension HomeInteractor: HomeInteractorInput {
+
+    func getListOfLaunches() {
+        repository.updateLaunches {
+            presenter?.getListOfLaunchesSuccess(list: repository.launches)
+        }
+    }
+}
